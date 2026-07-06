571b7a4b2a2f5cd8b0b5c5a6595360b2db23ed69
Tushar Das <tdas2663@gmail.com>
2026-07-06 21:35:42 +0530

    init biencoder uncommented

--- src/contrastors/models/biencoder/__init__.py
+++ src/contrastors/models/biencoder/__init__.py
@@ -1,4 +1,4 @@
-#from .configuration_biencoder import *
-#from .modeling_biencoder import *
+from .configuration_biencoder import *
+from .modeling_biencoder import *
 from .configuration_hyp_biencoder import *
 from .modeling_hyp_biencoder import *
